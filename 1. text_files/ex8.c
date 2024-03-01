8ebd214b135143a7b6f5baeadada07812ed71440
Lucas Maciel <03lucasmaciel@gmail.com>
2024-03-01 16:17:41 +0000

    feat add : ex8 completed

---
+++ 1. text_files/ex8.c
@@ -0,0 +1,6 @@
+/*
+ *
+ * 8. Implement the Unix cp command: The program should receive on the command line the name of the original file and the name of the final file and 
+ * should copy the contents of the 1st file to the 2nd, replacing all 'a' with '*' and 'o' with '_'.
+ *  
+ */
